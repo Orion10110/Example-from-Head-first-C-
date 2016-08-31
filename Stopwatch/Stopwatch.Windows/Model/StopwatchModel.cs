0283134b8196783fddf9931ad5ab7b6b34bc83ec
Orion10110 <pojasoriona@gmail.com>
2016-08-31 12:28:47 +0300

    Stopwatch v1

---
+++ Stopwatch/Stopwatch.Windows/Model/StopwatchModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch.Model
+{
+    class StopwatchModel
+    {
+        private DateTime? _started;
+
+        private TimeSpan? _previosElapsedTime;
+
+        public bool Running
+        {
+            get { return _started.HasValue; }
+        }
+
+        public TimeSpan? Elapsed
+        {
+            get
+            {
+                if (_started.HasValue)
+                {
+                    if (_previosElapsedTime.HasValue)
+                        return CalculateTimeElapsedSinceStarted() + _previosElapsedTime;
+                    else
+                        return CalculateTimeElapsedSinceStarted();
+                }
+                else
+                    return _previosElapsedTime;
+            }
+        }
+
+        public TimeSpan? LapTime { get; private set; }
+
+        private TimeSpan CalculateTimeElapsedSinceStarted()
+        {
+            return DateTime.Now - _started.Value;
+        }
+
+        public void Start()
+        {
+            _started = DateTime.Now;
+            if (!_previosElapsedTime.HasValue)
+                _previosElapsedTime = new TimeSpan(0);
+        }
+
+        public void Stop()
+        {
+            if (!_started.HasValue)
+                _previosElapsedTime += DateTime.Now - _started.Value;
+            _started = null;
+        }
+
+        public void Reset()
+        {
+            _previosElapsedTime = null;
+            _started = null;
+            LapTime = null;
+
+        }
+        public StopwatchModel()
+        {
+            Reset();
+        }
+
+        public void Lap()
+        {
+            LapTime = Elapsed;
+            OnLapTimeUpdated(LapTime);
+        }
+
+        public event EventHandler<LapEventArgs> LapTimeUpdater;
+
+        private void OnLapTimeUpdated(TimeSpan? lapTime){
+            EventHandler<LapEventArgs> lapTimeUpdated= LapTimeUpdater;
+            if(lapTimeUpdated != null){
+                lapTimeUpdated(this, new LapEventArgs(lapTime));
+            }
+        }
+
+
+    }
+}
---
+++ Stopwatch/Stopwatch.Windows/ViewModel/StopwatchViewModel.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using Windows.UI.Xaml;
+using Stopwatch.Model;
+
+namespace Stopwatch.ViewModel
+{
+    class StopwatchViewModel:INotifyPropertyChanged
+    {
+        private StopwatchModel _stopwatchModel = new StopwatchModel();
+
+        private DispatcherTimer _timer = new DispatcherTimer();
+
+        public bool Running { get { return _stopwatchModel.Running; } }
+
+        public StopwatchViewModel()
+        {
+            _timer.Interval = TimeSpan.FromMilliseconds(50);
+            _timer.Tick += _timer_Tick;
+            _timer.Start();
+            Start();
+
+            _stopwatchModel.LapTimeUpdater += LapTimeUpdatedEventHandler;
+        }
+
+        public void Start()
+        {
+            _stopwatchModel.Start();
+        }
+
+        public void Stop()
+        {
+            _stopwatchModel.Stop();
+        }
+        public void Lap()
+        {
+            _stopwatchModel.Lap();
+        }
+        public void Reset()
+        {
+            bool running = Running;
+            _stopwatchModel.Reset();
+            if (running)
+                _stopwatchModel.Start();
+        }
+        int _lastHours;
+        int _lastMinutes;
+        decimal _lastSeconds;
+        bool _lastRunning;
+   
+        void _timer_Tick(object sender, object e)
+        {
+            if (_lastRunning != Running)
+            {
+                _lastRunning = Running;
+                OnPropertyChanged("Running");
+            }
+
+            if (_lastHours != Hours)
+            {
+                _lastHours = Hours;
+                OnPropertyChanged("Hours");
+            }
+            if (_lastMinutes != Minutes)
+            {
+                _lastMinutes = Minutes;
+                OnPropertyChanged("Minutes");
+            }
+            if (_lastSeconds != Seconds)
+            {
+                _lastSeconds = Seconds;
+                OnPropertyChanged("Seconds");
+            }
+            
+        }
+
+        public int Hours
+        {
+            get { return _stopwatchModel.Elapsed.HasValue ? _stopwatchModel.Elapsed.Value.Hours : 0; }
+        }
+
+        public int Minutes
+        {
+            get { return _stopwatchModel.Elapsed.HasValue ? _stopwatchModel.Elapsed.Value.Minutes : 0; }
+        }
+
+        public decimal Seconds
+        {
+            get
+            {
+                if (_stopwatchModel.Elapsed.HasValue)
+                {
+                    return (decimal)_stopwatchModel.Elapsed.Value.Seconds
+                        + (_stopwatchModel.Elapsed.Value.Milliseconds * 0.001M);
+                }
+                else
+                {
+                    return 0.0M;
+                }
+            }
+        }
+
+        public int LapHours
+        {
+            get { return _stopwatchModel.LapTime.HasValue ? _stopwatchModel.LapTime.Value.Hours : 0; }
+        }
+
+        public int LapMinutes
+        {
+            get { return _stopwatchModel.LapTime.HasValue ? _stopwatchModel.LapTime.Value.Minutes : 0; }
+        }
+
+        public decimal LapSeconds
+        {
+            get
+            {
+                if (_stopwatchModel.LapTime.HasValue)
+                {
+                    return (decimal)_stopwatchModel.LapTime.Value.Seconds
+                        + (_stopwatchModel.LapTime.Value.Milliseconds * 0.001M);
+                }
+                else
+                {
+                    return 0.0M;
+                }
+            }
+        }
+
+        int _lastLapHourse;
+        int _lastLapMinutes;
+        decimal _lastLapSeconds;
+        private void LapTimeUpdatedEventHandler(object sender, LapEventArgs e)
+        {
+            if (_lastLapHourse != LapHours)
+            {
+                _lastLapHourse = LapHours;
+                OnPropertyChanged("LapHourse");
+            }
+            if (_lastLapMinutes != LapMinutes)
+            {
+                _lastLapMinutes = LapMinutes;
+                OnPropertyChanged("LapMinutes");
+            }
+            if (_lastLapSeconds != LapSeconds)
+            {
+                _lastLapSeconds = LapSeconds;
+                OnPropertyChanged("LapSeconds");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler properyChanged = PropertyChanged;
+            if (properyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
